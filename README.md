43f5ccc058b343799151520c3db5e291cf1288ff
Jared McGrath <jaredws35@gmail.com>
2019-01-18 01:43:26 -0500

    Update backend. See readme and segmentation.py for more details

--- README.md
+++ README.md
@@ -6,11 +6,53 @@ To get started:
 pip install pyAudioAnalysis matplotlib numpy
 ```
 
-There is a bug in pyAudioAnalysis/audioBasicIO.py, line 110:
+The meat & potatoes of this code is in `segmentation.py`, but first, you'll need to make some changes to the 
+pyAudioAnalysis library.
+
+## Modifying pyAudioAnalysis
+
+As only mono audio is required, a revision is made to `audioBasicIO.py`, line 110.
+This original code is:
 ```python
-if x.shape[1]==1:
-```
-This line needs to be changed to
+x = []
+for chn in list(range(audiofile.channels)):
+    x.append(data[chn::audiofile.channels])
+x = numpy.array(x).T
+```         
+Which becomes:
 ```python
-if x.shape[1]==2:
+x = numpy.array(data[0::audiofile.channels]).T
 ```
+
+
+When performing midterm segmentation, one will run into errors when the midterm window size and step are too little.
+This results because the original author of pyAudioAnalysis has hardcoded short term windows and steps of 50ms
+ for HMM training and 100ms for classification as
+part of the short term feature extraction. To get around this, all mentions of the `mtFeatureExtraction` function in 
+`audioFeatureExtraction.py` should be revised for increased flexibility.
+
+All function headers in `audioSegmentation.py` that call this function now include optional st_win and st_step parameters, 
+which are then used in the function call to `mtFeatureExtraction`.
+
+There's an annoying print statement in `audioSegmentation.py`, line 488. Recommend commenting it out.
+
+## Using the Project
+This project uses Hidden Markov Models (HMMs) to segment audio clips based on arbitrary categorizations. This is useful
+for not only classifying words, but also in identifying any characteristic in audio clips that can be labelled. An
+example of this is differentiating between speech and music. For our project, we will use the library to differentiate
+between regular and irregular patterns in speech. More specifically, we will remove portions of audio that have
+irregular cadence, then patching together the remaining valid audio so it can be more easily processed by traditional
+voice-to-text applications, such as Google Assistant.
+
+To accomplish this, an HMM is trained and then tested using annotated audio files. Audio files must be `.wav` format.
+Annotation files are plaintext `.segments` files that follow a comma separated value format as follows
+```text
+TIME_START1,TIME_END1,LABEL1
+TIME_START2,TIME_END2,LABEL2
+TIME_START3,TIME_END3,LABEL3
+...
+```
+The `.segments` file must share the same name as the `.wav` file. Label names are arbitrary, so long as they are
+consistent amongst portions of audio which share characteristics corresponding to the label.
+
+Place all training files and annotation in `/TrainHMM/`, and all testing files and annotation in `/TestHMM/`.
--- segmentation.py
+++ segmentation.py
@@ -1,13 +1,152 @@
 from pyAudioAnalysis import audioSegmentation as aS
+import glob
+import os
+import math
 
 # Directory where the annotated training data is located
-trainDir = 'ModernOTData/RawAnnotated'
+trainDir = 'ModernOTData/TrainHMM'
 # Name to store the HMM under
 hmmName = 'hmmTemp1'
-# Location of the test file
-testFile = 'ModernOTData/TrainHMM/light-off.wav'
-# Location of the .segments for the test file
-testFileSegments = 'ModernOTData/TrainHMM/light-off.segments'
+# Location of the test files, including annotations and .wav
+testDir = 'ModernOTData/TestHMM'
 
-aS.trainHMM_fromDir(trainDir, hmmName, 0.1, 0.1)
-aS.hmmSegmentation(testFile, hmmName, True, testFileSegments)
+
+# If we were to iterate through these parameters with different values for every short- and mid-term window step
+# and size, we would be able to optimize the HMM training for maximum accuracy
+# Depending on the training and test data, and what we're looking to optimize and segment for, we will be able to
+# Get a high accuracy in segmenting clips to remove irregular speech patterns depending on the speakers training data
+
+# Current proposal:
+# Short term window size could range between 20ms and 100ms, in intervals of 2-5ms
+# Call these stWinSizeMin and stWinSizeMax and stWinSizeInterval. The 20-100ms is based on research, the interval is not
+
+# Short term window step could vary such that there is no overlap (stWinSize) down to maybe 75% overlap, or more
+# So Short term window step could vary between stWinSize/4 and stWinSize, possibly with 10-50 intervals between
+# (Admittedly, the # of intervals I have no research to support what may be optimal, but empirical data can aid here)
+# If we were to use, say 20 intervals, the interval would be (stWinSizeMax - stWinSizeMin)/20
+# To generalize: stWinStepMin = stWinSize/stWinStepFactor, stWinStepMax = stWinSize,
+# stWinStepInterval = (stWinStepMax - stWinStepMin)/stWinStepNumIntervals
+
+# All of the above could be repeated for the midterm window variables
+# Then, iterate through parameters to find the highest accuracy
+# Also, once each set of parameters has been used to train the HMM, it should be tested on multiple files to avoid
+# overfitting.
+# Also, the parameters used to train the HMM might not be the best ones to test/use the HMM on, but I think this is
+# unlikely.
+
+# Another note: Our HMM data is pickled with mtWinSize and mtWinStep included. Maybe stWinSize and stWinStep should also
+# be included in the saved data. This way, aS.hmmSegmentation doesn't need the extra parameters
+
+# Once the HMM is trained, audio file editing must be addressed. As the hmmSegmentation function returns a list of
+# labels for an audio file, there should be an easy way to remove samples between certain times and save an edited
+# version of the audio file to the disk
+
+
+def optimizeParameters(stWinSizeMin=0.02, stWinSizeMax=0.05, stWinSizeInterval=0.005, stWinStepFactor=2,
+                       stWinStepNumIntervals=10, mtWinSizeMin=0.1, mtWinSizeMax=0.3, mtWinSizeInterval=0.02,
+                       mtWinStepFactor=2, mtWinStepNumIntervals=10):
+    """
+    This function does everything I outlined above. Currently too lazy to actually format it.
+    :param stWinSizeMin:
+    :param stWinSizeMax:
+    :param stWinSizeInterval:
+    :param stWinStepFactor:
+    :param stWinStepNumIntervals:
+    :param mtWinSizeMin:
+    :param mtWinSizeMax:
+    :param mtWinSizeInterval:
+    :param mtWinStepFactor:
+    :param mtWinStepNumIntervals:
+    :return:
+    """
+    # Constant to avoid weird floating point errors so that no configurations are skipped
+    floatError = 0.00000002
+    # Set defaults
+    mtWinSize = mtWinSizeMin
+    # Keep track of best accuracy
+    bestAcc = 0
+    bestConfig = []
+    print("Optimizing parameters. This will take a long time.")
+    # Loop for mtWinSize
+    while mtWinSize <= mtWinSizeMax + floatError:
+        # Initialize mtWinStep at mtWinSize (no overlap)
+        mtWinStep = mtWinSize/mtWinStepFactor
+        mtWinStepInterval = (mtWinSize - mtWinStep)/mtWinStepNumIntervals
+        # Loop for mtWinStep
+        while mtWinStep <= mtWinSize + floatError:
+            stWinSize = stWinSizeMin
+            # Loop for stWinSize
+            while stWinSize <= stWinSizeMax + floatError:
+                stWinStep = stWinSize / stWinStepFactor
+                stWinStepInterval = (stWinSize - stWinStep) / stWinStepNumIntervals
+                # Loop for stWinStep
+                while stWinStep <= stWinSize + floatError:
+                    try:
+                        # Parameters selected, now train HMM
+                        aS.trainHMM_fromDir(trainDir, hmmName, mtWinSize, mtWinStep, stWinSize, stWinStep)
+                        # Test HMM
+                        acc = testFromDir(testDir, hmmName, stWinSize, stWinStep)
+                        # If this config is better than the existing,
+                        if acc > bestAcc:
+                            bestAcc = acc
+                            bestConfig = (mtWinSize, mtWinStep, stWinSize, stWinStep)
+                    except ValueError:
+                        # Some configurations raise a value error. Haven't pinned down exactly which ones those are
+                        # But there is a pattern. Skipping such configs could optimize the processing
+                        print("Bad Config:")
+                        print(mtWinSize, mtWinStep, stWinSize, stWinStep)
+                    stWinStep += stWinStepInterval
+                stWinSize += stWinSizeInterval
+            mtWinStep += mtWinStepInterval
+        mtWinSize += mtWinSizeInterval
+
+    print("Best accuracy is {0:.2f}%".format(bestAcc*100))
+    print("Best config is:")
+    print("mtWinSize =", bestConfig[0])
+    print("mtWinStep =", bestConfig[1])
+    print("stWinSize =", bestConfig[2])
+    print("stWinStep =", bestConfig[3])
+    return bestConfig
+
+
+def testFromDir(path, hmmName, stWinSize, stWinStep, numFiles=-1):
+    """
+    Calculates the average segmentation accuracy of multiple test files in a directory.
+    Requires that all .wav files in the directory are annotated with an accompanying .segments file.
+    :param path: String
+        Path to the directory
+    :param hmmName: String
+        The name of the trained HMM
+    :param stWinSize: float
+        Short term window size, in seconds
+    :param stWinStep: float
+        Short term window step, in seconds
+    :param numFiles: int
+        (optional) The number of the files in the directory to test with. If not specified, all files will be tested.
+        If not all files are being used for testing, they will be randomly selected
+    :return: float
+        Average segmentation accuracy
+    """
+    if numFiles == -1:
+        allFiles = True
+    else:
+        allFiles = False
+
+    totalAcc = 0
+    totalFiles = 0
+    for i, f in enumerate(glob.glob(path + os.sep + '*.wav')):
+        if not allFiles and i >= numFiles:
+            break
+        # for each WAV file
+        wav_file = f
+        gt_file = f.replace('.wav', '.segments')
+        if not os.path.isfile(gt_file):
+            continue
+        _, _, acc, _ = aS.hmmSegmentation(wav_file, hmmName, False, gt_file, stWinSize, stWinStep)
+        totalAcc += acc
+        totalFiles += 1
+    return totalAcc/totalFiles
+
+
+optimizeParameters()
+
